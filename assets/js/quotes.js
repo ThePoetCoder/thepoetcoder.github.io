8a7f2a5314998236a6d9fc06c72b4070f59b7d03
ThePoetCoder <39857885+ThePoetCoder@users.noreply.github.com>
2022-09-11 16:35:26 -0500

    Update quotes.js

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -8,57 +8,57 @@ var quote;
 var link;
 
 switch (random_line) {
-    case 1: 
+    case 0: 
       quote = '"Fled is that music:—Do I wake or sleep?"  -Keats';
       link = 'https://www.poetryfoundation.org/poems/44479/ode-to-a-nightingale';
       break;
                 
-    case 2: 
+    case 1: 
       quote = '"Heard melodies are sweet, but those unheard are sweeter"  -Keats';
       link = 'https://www.poetryfoundation.org/poems/44477/ode-on-a-grecian-urn';
       break;
                 
-    case 3: 
+    case 2: 
       quote = '"\'Beauty is Truth, Truth Beauty,\'—that is all ye know on earth, and all ye need to know."  -Keats';
       link = 'https://www.poetryfoundation.org/poems/44477/ode-on-a-grecian-urn';
       break;
 
-    case 4: 
+    case 3: 
       quote = '"Love is not love which alters when it alteration finds"  -Shakespeare';
       link = 'https://www.poetryfoundation.org/poems/45106/sonnet-116-let-me-not-to-the-marriage-of-true-minds';
       break;
                 
-    case 5: 
+    case 4: 
       quote = '"Water can be boiled in a pot, or if you have no pot, over a fire in your cupped hands."  -Jollimore';
       link = 'https://www.troyjollimore.com/';
       break;
 
-    case 6: 
+    case 5: 
       quote = '"Is this world other than what I see?"  -Adonis';
       link = 'https://www.poetryfoundation.org/poets/adonis';
       break;
                 
-    case 7: 
+    case 6: 
       quote = '"I\'ll tell you what I\'ll inherit: the margins"  -Doty';
       link = 'https://www.poetryfoundation.org/poems/44139/homo-will-not-inherit';
       break;
                 
-    case 8: 
+    case 7: 
       quote = '"Without thinking at all I was my foolish aunt, I—we—were falling, falling"  -Bishop';
       link = 'https://poets.org/poem/waiting-room';
       break;
 
-    case 9: 
+    case 8: 
       quote = '"In this short life That only lasts an hour How much — how little — is within our power?"  -Dickinson';
       link = 'https://www.poetryfoundation.org/poems/56456/in-this-short-life-that-only-lasts-an-hour-1292';
       break;
                     
-    case 10: 
+    case 9: 
       quote = '"You\'re thinking about this too much. Slow down. Nothing bad will happen."  -Prufer';
       link = 'https://www.poetryfoundation.org/poetrymagazine/poems/52972/in-a-beautiful-country';
       break;
                     
-    case 11: 
+    case 10: 
       quote = '"Let us go then, you and I, When the evening is spread out against the sky"  -Eliot';
       link = 'https://www.poetryfoundation.org/poetrymagazine/poems/44212/the-love-song-of-j-alfred-prufrock';
       break;
