876d710d6d1962f492eafd17eb8a2670e1caddac
ThePoetCoder <39857885+ThePoetCoder@users.noreply.github.com>
2022-09-11 11:07:17 -0500

    Update quotes.js

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,28 +1,73 @@
-var poetry = [
+var total_lines = 11;
+var random_line = Math.floor(Math.random() * total_lines);
 
-        '"Fled is that music:—Do I wake or sleep?"  -Keats',
+var quote_element = document.getElementById("quote");
+var quote_link = document.getElementById("quote-link");
 
-        '"Heard melodies are sweet, but those unheard are sweeter"  -Keats',
+var quote;
+var link;
 
-        '"\'Beauty is Truth, Truth Beauty,\'—that is all ye know on earth, and all ye need to know."  -Keats',
+switch (random_line) {
+    case 1: 
+      quote = '"Fled is that music:—Do I wake or sleep?"  -Keats';
+      link = 'https://www.poetryfoundation.org/poems/44479/ode-to-a-nightingale';
+      break;
+                
+    case 2: 
+      quote = '"Heard melodies are sweet, but those unheard are sweeter"  -Keats';
+      link = 'https://www.poetryfoundation.org/poems/44477/ode-on-a-grecian-urn';
+      break;
+                
+    case 3: 
+      quote = '"\'Beauty is Truth, Truth Beauty,\'—that is all ye know on earth, and all ye need to know."  -Keats';
+      link = 'https://www.poetryfoundation.org/poems/44477/ode-on-a-grecian-urn';
+      break;
 
-        '"Love is not love which alters when it alteration finds"  -Shakespeare',
-        
-        '"Water can be boiled in a pot, or if you have no pot, over a fire in your cupped hands."  -Jollimore',
-        
-        '"Is this world other than what I see?"  -Adonis',
-        
-        '"I\'ll tell you what I\'ll inherit: the margins"  -Doty',
-        
-        '"Without thinking at all I was my foolish aunt, I—we—were falling, falling"  -Bishop',
-        
-        '"In this short life That only lasts an hour How much — how little — is thin our power?"  -Dickinson',
-        
-        '"You\'re thinking about this too much. Slow down. Nothing bad will happen."  -Prufer',
-        
-        '"Let us go then, you and I, When the evening is spread out against the sky"  -Eliot'
-        ];
+    case 4: 
+      quote = '"Love is not love which alters when it alteration finds"  -Shakespeare';
+      link = 'https://www.poetryfoundation.org/poems/45106/sonnet-116-let-me-not-to-the-marriage-of-true-minds';
+      break;
+                
+    case 5: 
+      quote = '"Water can be boiled in a pot, or if you have no pot, over a fire in your cupped hands."  -Jollimore';
+      link = 'https://www.troyjollimore.com/';
+      break;
 
-var line = poetry[Math.floor(Math.random() * poetry.length)];
+    case 6: 
+      quote = '"Is this world other than what I see?"  -Adonis';
+      link = 'https://www.poetryfoundation.org/poets/adonis';
+      break;
+                
+    case 7: 
+      quote = '"I\'ll tell you what I\'ll inherit: the margins"  -Doty';
+      link = 'https://www.poetryfoundation.org/poems/44139/homo-will-not-inherit';
+      break;
+                
+    case 8: 
+      quote = '"Without thinking at all I was my foolish aunt, I—we—were falling, falling"  -Bishop';
+      link = 'https://poets.org/poem/waiting-room';
+      break;
 
-document.getElementById("quote").innerHTML = line;
+    case 9: 
+      quote = '"In this short life That only lasts an hour How much — how little — is within our power?"  -Dickinson';
+      link = 'https://www.poetryfoundation.org/poems/56456/in-this-short-life-that-only-lasts-an-hour-1292';
+      break;
+                    
+    case 10: 
+      quote = '"You\'re thinking about this too much. Slow down. Nothing bad will happen."  -Prufer';
+      link = 'https://www.poetryfoundation.org/poetrymagazine/poems/52972/in-a-beautiful-country';
+      break;
+                    
+    case 11: 
+      quote = '"Let us go then, you and I, When the evening is spread out against the sky"  -Eliot';
+      link = 'https://www.poetryfoundation.org/poetrymagazine/poems/44212/the-love-song-of-j-alfred-prufrock';
+      break;
+
+    default:
+      quote = '"Fled is that music:—Do I wake or sleep?"  -Keats';
+      link = 'https://www.poetryfoundation.org/poems/44479/ode-to-a-nightingale';
+      break;
+}
+
+quote_element.innerHTML = quote;
+quote_link.setAttribute('href', link);
